5891e7de39b296b297d49a5bc5cc680a34a947c9
KamilTokarz110 <56870559+BialaMewa110@users.noreply.github.com>
2020-10-30 10:54:09 +0100

    After @mediaquery

--- main.js
+++ main.js
@@ -1,128 +1,3 @@
-// $('nav a').on('click', function(){
-//   const goToSection =  "[data-section" + $(this).attr('class')} + "]";
- 
-  
-//   console.log(goToSection);
-
-//   $('body, html').animate({
-//       scrollTop: $(goToSection).offset().top
-//   })
-    
-
-// })
-//Dzziała
-// $(window).scroll(function() {
-//     var windscroll = $(window).scrollTop();
-//     if (windscroll >= 100) {
-//         $('nav').addClass('menu--scrolled');
-//         $('.wrapper section').each(function(i) {
-//             if ($(this).position().top <= windscroll - 100) {
-//                 $('nav menu--scrolled').removeClass('menu--scrolled');
-//                 $('nav li').eq(i).addClass('menu__item--active');
-//             }
-//         });
-
-//     } else {
-
-//         $('nav').removeClass('menu--scrolled');
-//         $('nav li.menu__item--active').removeClass('menu__item--active');
-//         $('nav li:first').addClass('menu__item--active');
-//     }
-
-// }).scroll()
-
-
-   
-
-// smoothScroll()
-
-// let mainNavLinks = document.querySelectorAll("nav ul li a");
-// let mainSections = document.querySelectorAll("section");
-
-// let lastId;
-// let cur = [];
-
-// window.addEventListener("scroll", event =>{
-//     let fromTop = window.scrollY;
-
-//     mainNavLinks.forEach(link =>{
-//         let section = document.querySelector(link.hash);
-
-//         if(
-//             section.offsetTop <= fromTop &&
-//             section.offsetTop + section.offSetHeight > fromTop
-//         ){
-//             link.classList.add("menu__item--active");
-//         } else {
-//             link.classList.remove("menu__item--active");
-//         }
-//     });
-// });
-
-// $(document).ready(function(){
-//     $('a[href*=#]').bind('click', function(e){
-//         e.preventDefault();
-
-//         var target = $(this).attr("href");
-
-//         $('html, body').stop().animate({
-//             scrollTop: $(target).offset().top
-//         }, 600, function(){
-//             location.hash = target;
-//         });
-
-//         return false;
-//     });
-// });
-// $(window).scroll(function(){
-//     var scrollDistance = $(window).scrollTop();
-//     $('.page-section').each(function(i){
-//         if($(this).position().top <= scrollDistance){
-//             $('.menu .menu__item--active').removeClass('menu__item--active');
-//             $('.menu .menu__item').addClass('menu__item--active');
-//         };
-//     });
-// }).scroll();
-
-// document.addEventListener('DOMContentLoaded', function (){
-//     console.log("Witaj !");
-
-//     const rootElement = document.querySelector('#root');
-//     const sections = document.querySelectorAll('section');
-//     let currentSectionIndex = 0;
-//     let isThrottled = false;
-
-//     setTimeout(function(){
-//         isThrottled = false;
-//     },1000);
-
-//     document.addEventListener('mousewheel', function (event) {
-       
-//         if(isThrottled) return;
-//         isThrottled = true;
-
-//         const direction = event.wheelDelta < 0 ? 1 : -1;
-
-//        if(direction === 1){
-//            const isLastSection = currentSectionIndex === sections.length -1;
-//            if(isLastSection) return;
-//        } else if(direction === -1){
-//            const isFirstSection = currentSectionIndex === 0;
-//            if(isFirstSection) return;
-//        }
-
-//        currentSectionIndex = currentSectionIndex + direction;
-
-//        console.log(currentSectionIndex);
-
-//        sections[currentSectionIndex].scrollIntoView({
-//            behavior: 'smooth',
-//            block: 'start',
-//        })
-
-//     })
-// })
-
 $(document).ready(function() {
     $(window).scroll(function() {
 
@@ -144,10 +19,10 @@ $(document).ready(function() {
                 navContact = $("#nav6");
             var navigation = $(".menu");    
 
-            if(scrollPosition >= homePosition){
+            if(scrollPosition >= homePosition +100){
                 navigation.addClass("menu--scrolled");
             } 
-             if(scrollPosition <= homePosition) {
+             if(scrollPosition <= homePosition + 700) {
                 navigation.removeClass("menu--scrolled");
             }
             if(scrollPosition >= homePosition ){
@@ -176,50 +51,10 @@ $(document).ready(function() {
                 navContact.addClass("menu__item--active");
             } 
    
-    $(".menu__item").click(function(){
-        $(this).removeClass("menu__item--active");
-        $(this).addClass("menu__item--active");
+   
+});
+});
 
-    });
-});
-});
-//     // Select all links with hashes
-// $('a[href*="#"]')
-// // Remove links that don't actually link to anything
-// .not('[href="#"]')
-// .not('[href="#0"]')
-// .click(function(event) {
-//   // On-page links
-//   if (
-//     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-//     && 
-//     location.hostname == this.hostname
-//   ) {
-//     // Figure out element to scroll to
-//     var target = $(this.hash);
-//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//     // Does a scroll target exist?
-//     if (target.length) {
-//       // Only prevent default if animation is actually gonna happen
-//       event.preventDefault();
-//       $('html, body').animate({
-//         scrollTop: target.offset().top
-//       }, 1000, function() {
-//         // Callback after animation
-//         // Must change focus!
-//         var $target = $(target);
-//         $target.focus();
-//         if ($target.is(":focus")) { // Checking if the target was focused
-//           return false;
-//         } else {
-//           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-//           $target.focus(); // Set focus again
-//         };
-//       });
-//     }
-//   }
-// });
-// });
       
 
             
