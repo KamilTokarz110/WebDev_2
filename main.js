c62844aaeddabc82dcfd00a38c2ab5b7e463e8e2
KamilTokarz110 <56870559+BialaMewa110@users.noreply.github.com>
2020-11-01 14:06:19 +0100

    Remove JQuery and add JavaScript scroll function

--- main.js
+++ main.js
@@ -1,62 +1,50 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
 
-        var scrollPosition = $(window).scrollTop();
-        //Positions of the containters
-        var homePosition = $("#home").offset().top,
-            aboutPosition = $('#about').offset().top,
-            servicesPositon = $('#services').offset().top,
-            galleryPosition = $('#gallery').offset().top,
-            //subscribePosition = $('#subscribe').offset().top,
-            blogPosition = $('#blog').offset().top,
-            contactPosition = $("#contact").offset().top;
-            //get the nav elements
-            var navHome = $("#nav1"),
-                navAbout = $("#nav2"),
-                navServices = $("#nav3"),
-                navGallery = $("#nav4"),
-                navBlog = $("#nav5"),
-                navContact = $("#nav6");
-            var navigation = $(".menu");    
+const navigation = document.querySelector("#navigation");
+const sectionOne = document.querySelector(".header");
 
-            if(scrollPosition >= homePosition +100){
-                navigation.addClass("menu--scrolled");
-            } 
-             if(scrollPosition <= homePosition + 700) {
-                navigation.removeClass("menu--scrolled");
+
+
+const sectionOneOptions = {
+    rootMargin: "-300px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries,
+    sectionOneObserver
+){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting) {
+            navigation.classList.add("menu--scrolled");
+        } else{
+            navigation.classList.remove("menu--scrolled");
+        }
+    });
+
+
+},sectionOneOptions);
+sectionOneObserver.observe(sectionOne);
+
+window.addEventListener('scroll', event => {
+    let navigationLinks = document.querySelectorAll('nav ul li a');
+    let fromTop = window.scrollY;
+
+    navigationLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if(
+            section.offsetTop <= fromTop && 
+            section.offsetTop + section.offsetHeight > fromTop
+            ) {
+            
+                link.classList.add('menu__link--active');
+        
+            } else {
+                link.classList.remove('menu__link--active');
             }
-            if(scrollPosition >= homePosition ){
-                navHome.siblings().removeClass("menu__item--active");
-                navHome.addClass("menu__item--active");
-                
-            }  
-            if(scrollPosition >= aboutPosition ){
-                navAbout.siblings().removeClass("menu__item--active");
-                navAbout.addClass("menu__item--active");
-            } 
-            if(scrollPosition >= servicesPositon ){
-                navServices.siblings().removeClass("menu__item--active");
-                navServices.addClass("menu__item--active");
-            } 
-            if(scrollPosition >= galleryPosition ){
-                navGallery.siblings().removeClass("menu__item--active");
-                navGallery.addClass("menu__item--active");
-            } 
-            if(scrollPosition >= blogPosition ){
-                navBlog.siblings().removeClass("menu__item--active");
-                navBlog.addClass("menu__item--active");
-            } 
-            if(scrollPosition >= contactPosition ){
-                navContact.siblings().removeClass("menu__item--active");
-                navContact.addClass("menu__item--active");
-            } 
-   
-   
+        
+        
 });
 });
-
-      
-
             
 
 
